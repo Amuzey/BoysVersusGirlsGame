1ea16ec56e4f49f099e2a8ec3b1cbae62129bab8
Алексей Иванов <ivanovad97@gmail.com>
2022-10-19 19:22:20 +0500

    create SetVC create RulesVC create StartVC delete StoryBoard

--- BoysVersusGirlsGame/StartViewController.swift
+++ BoysVersusGirlsGame/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  BoysVersusGirlsGame
 //
 //  Created by Алексей on 19.10.2022.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "www"
+        view.backgroundColor = .red
     }
 
 
